6bd83bfbdc1d775a6bf28108002d6602109f5611
Nout Kleef <noutkleef1@gmail.com>
2021-03-08 16:34:37 +0000

    add dropdown to select theme

--- app/src/main/java/com/helio/app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/helio/app/ui/settings/SettingsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,8 +19,13 @@ public class SettingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        Spinner themeSpinner = (Spinner) view.findViewById(R.id.theme_selection_dropdown);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                view.getContext(), R.array.theme_options, android.R.layout.simple_spinner_item
+        );
+        themeSpinner.setAdapter(adapter);
+        return view;
     }
 }

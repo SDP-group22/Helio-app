33d6d292c3a2c9e640fa73e8d0b8555e3c6c0bd7
TianYouX <77977523+TianYouX@users.noreply.github.com>
2021-03-10 15:08:23 +0800

    voice integration
    Start RecognizerIntent when the floating action button is clicked. Add method for receiving the speech response.

--- app/src/main/java/com/helio/app/ui/control/ControlFragment.java
+++ app/src/main/java/com/helio/app/ui/control/ControlFragment.java
@@ -1,24 +1,35 @@
 package com.helio.app.ui.control;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.helio.app.R;
 import com.helio.app.UserDataViewModel;
+import com.helio.app.model.Motor;
 
 import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ControlFragment extends Fragment {
 
     private UserDataViewModel model;
+    protected static final int RESULT_SPEECH = 100;
+    private FloatingActionButton fab_voiceIntegration;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +41,23 @@ public class ControlFragment extends Fragment {
                 motors -> adapter.setMotors(new ArrayList<>(motors.values()))
         );
 
+        // Start RecognizerIntent
+        fab_voiceIntegration = view.findViewById(R.id.fab_voice_integration);
+        fab_voiceIntegration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please start your voice");
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                } catch (ActivityNotFoundException a) {
+                    Toast t = Toast.makeText(getActivity(),
+                            "Opps! Your device doesn't support Speech to Text",
+                            Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
 
 
         // Insert into the recycler view
@@ -38,4 +66,25 @@ public class ControlFragment extends Fragment {
         recView.setLayoutManager(new LinearLayoutManager(getContext()));
         return view;
     }
+
+    // Receiving the speech response
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    // Take action
+                    model.voiceIntegrationAction(text.get(0));
+//                    Toast t = Toast.makeText(getActivity(), text.get(0), Toast.LENGTH_LONG);
+//                    t.show();
+                }
+                break;
+            }
+
+        }
+    }
 }
+

e11428f6e4d0c8fae62f17d2da1730902b3a43be
Adam <adamb56789@gmail.com>
2021-03-12 15:28:17 +0000

    Formatted and optimized imports

--- app/src/main/java/com/helio/app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/helio/app/ui/settings/SettingsFragment.java
@@ -1,22 +1,15 @@
 package com.helio.app.ui.settings;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ListAdapter;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.helio.app.MainActivity;
@@ -50,9 +43,9 @@ public class SettingsFragment extends Fragment {
         ListAdapter adapter = Objects.requireNonNull(themeMenu).getAdapter();
         String currentThemeName = ((MainActivity) requireActivity()).getCurrentThemeName();
         final int count = adapter.getCount();
-        for(int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             String itemValue = (String) adapter.getItem(i);
-            if(itemValue.equals(currentThemeName)) {
+            if (itemValue.equals(currentThemeName)) {
                 System.out.println("Setting spinner to \"" + itemValue + "\"...");
                 themeMenu.setText(currentThemeName, false);
                 break;

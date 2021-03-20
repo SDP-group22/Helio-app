5da68e77dc45a51ab49243ae1d52d2eb176acc95
Nout Kleef <noutkleef1@gmail.com>
2021-03-20 12:58:38 +0000

    optimise imports

--- app/src/androidTest/java/com/helio/app/TestSensors.kt
+++ app/src/androidTest/java/com/helio/app/TestSensors.kt
@@ -5,18 +5,15 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest

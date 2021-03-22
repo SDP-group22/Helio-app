10a6af0506d76bda2ee05efd559b8ebde1954c49
Nout Kleef <noutkleef1@gmail.com>
2021-03-22 13:58:26 +0000

    abstract away IP setting logic

--- app/src/androidTest/java/com/helio/app/TestSettings.kt
+++ app/src/androidTest/java/com/helio/app/TestSettings.kt
@@ -82,16 +82,7 @@ class TestSettings {
     // check that we can update the IP address value to a new, correctly formatted IP
     fun updateHubIPValid() {
         val ip = "1.2.8.9"
-        // update the IP
-        onView(withId(R.id.ip_address))
-                .perform(ViewActions.click())
-        onView(withId(R.id.ip_address_edittext))
-                .perform(ViewActions.clearText())
-                .perform(ViewActions.typeTextIntoFocusedView(ip))
-        // attempt to connect
-        onView(withId(R.id.connect_button))
-                .perform(ViewActions.click())
-        Thread.sleep(2000)
+        setHubIP(ip)
         // check that IP is correctly set
         onView(withId(R.id.ip_address))
                 .check(matches(Utils.withText(ip)))
@@ -123,6 +114,18 @@ class TestSettings {
                 .perform(ViewActions.click())
         onView(withText(themeName))
                 .inRoot(RootMatchers.isPlatformPopup())
+                .perform(ViewActions.click())
+    }
+
+    private fun setHubIP(ip: String) {
+        // update the IP
+        onView(withId(R.id.ip_address))
+                .perform(ViewActions.click())
+        onView(withId(R.id.ip_address_edittext))
+                .perform(ViewActions.clearText())
+                .perform(ViewActions.typeTextIntoFocusedView(ip))
+        // attempt to connect
+        onView(withId(R.id.connect_button))
                 .perform(ViewActions.click())
     }
 }

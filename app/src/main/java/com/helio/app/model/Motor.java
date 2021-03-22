ac96d114529be73e83c878a2ffd70736a83e2541
TianYouX <77977523+TianYouX@users.noreply.github.com>
2021-03-22 23:35:15 +0800

    delete unused method

--- app/src/main/java/com/helio/app/model/Motor.java
+++ app/src/main/java/com/helio/app/model/Motor.java
@@ -12,7 +12,7 @@ public class Motor implements IdComponent {
     private final int length;
     private String name;
     private String ip;
-    private boolean active;
+    private final boolean active;
     private int level;
     private String style;
 
@@ -84,9 +84,6 @@ public class Motor implements IdComponent {
         return active;
     }
 
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 
     public int getLevel() {
         return level;
@@ -106,10 +103,6 @@ public class Motor implements IdComponent {
 
     public String getStyle() {
         return style;
-    }
-
-    public void setStyle(String style) {
-        this.style = style;
     }
 
     public int getIconId() {
